ab39e5b91fd8fea8b3741c0fc6dbef774b6e4c9b
Srividya Katam <6183423+srividyakatam@users.noreply.github.com>
2024-04-19 15:15:59 -0700

    client

---
+++ client/src/components/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/About.css";
+
+export const About = () => {
+  
+  return (
+    <>
+      <div className="About">
+        
+      </div>
+      
+    </>
+  );
+};
